44aac1f466ca6b2e59458bb77d4d8172afb7180d
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-24 21:17:36 -0300

    working

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
-
 #!/bin/bash
-
 
 # Obtém o diretório do script
 DIR="$(dirname "$(realpath "$0")")"
 LIB_PATH="$DIR/lib"
+
 # Adiciona o diretório ao LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$LIB_PATH:$LD_LIBRARY_PATH"
-BIN_PATH="$DIR/tesseract_bin"
-"$BIN_PATH" "${@:2}"
+
+# Remove o primeiro argumento (script) e obtém os outros
+COMMAND="$DIR/tesseract_bin ${@:1}"
+
+eval $COMMAND
